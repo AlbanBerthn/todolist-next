cfbf138ecbcc82bc81a0283e8b7b6961907e6ccf
AlbanBerthn <a.bertholon69@gmail.com>
2023-11-12 21:59:03 +0100

    essais pour compréhension du routing + ajout de sass via yarn et test de mise en couleur du header et footer

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import "./footer.scss";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <h1>Hello, je suis le footer!</h1>
+    </div>
+  );
+}
---
+++ src/app/Task/page.tsx
@@ -0,0 +1,3 @@
+export default function Task() {
+  return <h1>Je suis le composant tasks!</h1>;
+}
