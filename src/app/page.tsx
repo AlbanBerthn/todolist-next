e240186de8c6b6ce75aa9c9a7e2775be114a6c3a
AlbanBerthn <a.bertholon69@gmail.com>
2023-11-13 23:25:23 +0100

    mise en place de la page d'accueil et redirection du bouton sur l'url de la page Task

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,37 @@
+"use client";
+import { useRouter } from "next/navigation";
+import "./page.scss";
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  const router = useRouter();
+  function handleClick() {
+    router.push("/Task");
+  }
+  return (
+    <div className="presentation">
+      <h3 className="presentation__title">
+        Marre d&apos;être tête en l&apos;air ?
+      </h3>
+      <p className="presentation__description">
+        Le but de notre site est de permettre a tout personne d&apos;acceder
+        facilement <br />
+        et de n&apos;importe ou a sa liste de tache a faire afin de ne rien
+        oublier. <br />
+        <br />
+        Il est également possible de faire sa liste de courses avec les
+        quantités désirés pour chaques élement de cette liste. <br />
+        <br />
+        Alors n&apos;hésiter plus, cliquer sur l&apos;onglet tâche, rentrer vos
+        tâches et a vous la liberté et la possibilité de temporairements les
+        oubliers.
+        <br />
+        <br />
+        Notre site y pensera pour vous !
+      </p>
+
+      <button className="presentation__button" onClick={handleClick}>
+        Je créer ma première tache !!
+      </button>
+    </div>
+  );
 }
