fe5fc9b02ca263af5e1a446a980e817a766e417e
AlbanBerthn <a.bertholon69@gmail.com>
2023-11-15 17:37:04 +0100

    mise en place de la page des taches, mise en place de l'input pour ajouter une tache

--- src/app/Task/page.tsx
+++ src/app/Task/page.tsx
@@ -1,3 +1,33 @@
+"use client";
+import { useState } from "react";
+import "./task.scss";
+
 export default function Task() {
-  return <h1>Je suis le composant tasks!</h1>;
+  const [task, setTask] = useState("");
+
+  function handleChange(event: any) {
+    setTask(event.target.value);
+  }
+  return (
+    <div className="task">
+      <div className="task__form">
+        <label className="task__form--label" htmlFor="task">
+          {" "}
+          Ajouter une tâche
+        </label>
+        <input
+          className="task__form--input"
+          type="text"
+          placeholder="Entrez une tâche..."
+          id="task"
+          value={task}
+          onChange={handleChange}
+        ></input>
+        <button className="task__form--button">Ajouter</button>
+      </div>
+      <div className="task__list">
+        <ul className="task__list_items"></ul>
+      </div>
+    </div>
+  );
 }
