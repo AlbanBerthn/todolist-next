e9801d534f42ba0d07cffcbc37377a6ef72da181
AlbanBerthn <a.bertholon69@gmail.com>
2023-11-15 21:20:27 +0100

    gestion du css de la liste des taches, qui n'apparaissent plus depuis que j'ai ajouter un bouton qui reagis au clique pour supprimer la tache

--- src/app/Task/page.tsx
+++ src/app/Task/page.tsx
@@ -1,13 +1,28 @@
 "use client";
+import React from "react";
 import { useState } from "react";
 import "./task.scss";
 
 export default function Task() {
   const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState<{ id: number; task: string }[]>([]);
 
   function handleChange(event: any) {
     setTask(event.target.value);
   }
+
+  function addTask() {
+    setTaskList((prev) => {
+      return [...prev, { id: prev.length + 1, task: task }];
+    });
+    setTask("");
+  }
+
+  function handleDelete(id: number) {
+    const newTaskList = taskList.filter((item) => item.id != id);
+    setTaskList(newTaskList);
+  }
+  // console.log(taskList);
   return (
     <div className="task">
       <div className="task__form">
@@ -23,10 +38,29 @@ export default function Task() {
           value={task}
           onChange={handleChange}
         ></input>
-        <button className="task__form--button">Ajouter</button>
+        {/* Ce qui suis permet d'afficher le bouton uniquement si le champ 
+        input contient au moins UN caractère */}
+        {task && (
+          <button className="task__form--button" onClick={addTask}>
+            Ajouter
+          </button>
+        )}
       </div>
       <div className="task__list">
-        <ul className="task__list_items"></ul>
+        <h3 className="task__list--title">Liste des tâches</h3>
+        <ul className="task__list--items">
+          {taskList.map((item, index) => (
+            <li className="task__list--item" key={index}>
+              {item.task}{" "}
+              <button
+                className="task__list__item--button"
+                onClick={handleDelete(item.id)}
+              >
+                Supprimer
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
